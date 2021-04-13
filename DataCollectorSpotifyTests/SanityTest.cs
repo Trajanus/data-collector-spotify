8dbe12bbde91738e1d93518af253571088fff9e0
Trajanus <derektgoodwin@gmail.com>
2021-04-13 10:21:09 -0700

    inital commit of empty unit testing framework and console application.

---
+++ DataCollectorSpotifyTests/SanityTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DataCollectorSpotifyTests
+{
+    public class SanityTest
+    {
+        [Fact]
+        public void TestFrameworkWorking_True_AssetsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
